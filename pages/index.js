10dbc19e6cf3522be6db73e0aa52d19a033ec520
Damián Silvani <munshkr@gmail.com>
2019-07-24 16:24:34 -0300

    GET gists on page load

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import axios from "axios";
 
-function Home() {
-  return <div>Welcome to Next.js!</div>;
+class Home extends React.Component {
+  static async getInitialProps({ query }) {
+    return { query };
+  }
+
+  componentDidMount() {
+    const { query } = this.props;
+    const user = query.u || "munshkr";
+    this.getGistsFrom(user);
+  }
+
+  async getGistsFrom(user) {
+    console.log(`Fetch gists from ${user}`);
+
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${user}/gists`
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Failed to fetch gists. Github API down?");
+      console.error(error);
+    }
+  }
+
+  render() {
+    return <div>Welcome to Next.js!</div>;
+  }
 }
 
 export default Home;
